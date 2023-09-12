39c69a0d218e12bab12ab9e3126619ba86bb7433
Sam Louloudis <samanthaleelouloudis@gmail.com>
2023-09-12 05:58:04 +0100

    sticky

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -3,21 +3,20 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import styles from './styles.module.css'
 
 
-const Page = ({ offset, image, onClick }) => (
+const Page = ({ offset, image, onClick, title}) => (
   <>
     <ParallaxLayer offset={offset} speed={0.2} onClick={onClick}>
       <div className={styles.slopeBegin} />
     </ParallaxLayer>
-
     <ParallaxLayer offset={offset} speed={0.6} onClick={onClick}>
       <div style={{ backgroundImage: `url(${image})`,
-                    backgroundSize: 'contain',
+                    backgroundSize: 'auto',
                     backgroundPosition: 'center',
-     }} className={styles.slopeEnd} />
+                  }} className={styles.slopeEnd} />
     </ParallaxLayer>
 
     <ParallaxLayer className={`${styles.text} ${styles.number}`} offset={offset} speed={0.3}>
-      <span>0{offset + 1}</span>
+      <span>{title}</span>
     </ParallaxLayer>
   </>
 )
@@ -34,9 +33,12 @@ export default function App() {
   return (
     <div style={{ background: '#dfdfdf' }}>
       <Parallax className={styles.container} ref={parallax} pages={3} horizontal>
-        <Page offset={0} image="/flower.jpg" onClick={() => scroll(1)} />
-        <Page offset={1} image="/lilly.jpg" onClick={() => scroll(2)} />
-        <Page offset={2} image="/otter.jpg" onClick={() => scroll(0)} />
+        <Page offset={0} image="/flower.jpg" onClick={() => scroll(1)} title={'flowers'} />
+        <Page offset={1} image="/lilly.jpg" onClick={() => scroll(2)} title={' Lillies'} />
+        <Page offset={2} image="/otter.jpg" onClick={() => scroll(0)} title={'Otters'} />
+        <ParallaxLayer sticky={{ start: 0 , end: 1 }} offset={1} className={`${styles.sticky} ${styles.number}`}>
+          <p>{'sticky!'}</p>
+        </ParallaxLayer>
       </Parallax>
     </div>
   )
